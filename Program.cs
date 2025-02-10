4ec03929c4d36874cad1045d4466958391f15649
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-02-11 01:16:49 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,45 @@
-﻿rsdfrvsf 
+﻿string[] fraudulentOrderIDs = new string[3];
+
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+
+string[] fraudulentIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentIDs[0]}");
+Console.WriteLine($"Second: {fraudulentIDs[1]}");
+Console.WriteLine($"Third: {fraudulentIDs[2]}");
+
+fraudulentIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentIDs[0]}");
+Console.WriteLine($"There are {fraudulentIDs.Length} fraudulent orders to process.");
+
+//LOOPING
+
+string[] names = {"Rowena", "Robin", "Bao"};
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
+{
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} (Running total: {sum})");
+}
+Console.WriteLine($"We have {sum} items in inventory");
+
+string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+foreach (string id in orderIDs)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine(id);
+    }
+}
--- obj/Debug/net8.0/arraysInCsharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/arraysInCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("arraysInCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0872a56497a40031813f0c578a8324431c3354")]
 [assembly: System.Reflection.AssemblyProductAttribute("arraysInCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("arraysInCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
